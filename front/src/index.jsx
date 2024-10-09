5f6f2bc3089ac139dedafdd2883363dd1a945238
luc perrot <luc.perrot@hotmail.fr>
2024-10-09 15:25:46 +0200

    better architecture

---
+++ front/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import SignIn from './pages/SignIn';
+import User from './pages/User'
+import MainNav from './components/MainNav';
+import Footer from './components/Footer';
+import './App.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <MainNav />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/user" element={<User />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  </React.StrictMode>
+);
