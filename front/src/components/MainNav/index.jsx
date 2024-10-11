b662b769ed5929528eabfadeca3774e77ecae686
luc perrot <luc.perrot@hotmail.fr>
2024-10-11 11:14:01 +0200

    logout feature

--- front/src/components/MainNav/index.jsx
+++ front/src/components/MainNav/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../assets/img/argentBankLogo.png';
-import './MainNav.css'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/img/argentBankLogo.png";
+import { authSlice } from "../../pages/SignIn/authSlice";
+import "./MainNav.css";
 
 const MainNav = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { token } = useSelector((state) => state.auth);
+  const { firstName } = useSelector((state) => state.user);
+
+  const handleLogout = () => {
+    dispatch(authSlice.actions.logoutUser());
+    navigate("/");
+  };
+
   return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
@@ -11,10 +23,23 @@ const MainNav = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
-        <Link className="main-nav-item" to="/login">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </Link>
+        {token ? (
+          <div>
+            <Link className="main-nav-item" to="/profile">
+              <i className="fa fa-user-circle"></i>
+              {firstName}
+            </Link>
+            <Link className="main-nav-item" to="/" onClick={handleLogout}>
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </Link>
+          </div>
+        ) : (
+          <Link className="main-nav-item" to="/login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        )}
       </div>
     </nav>
   );
