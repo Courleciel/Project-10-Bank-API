dc02934054da0f892629c994894d96b138dd0e20
luc perrot <luc.perrot@hotmail.fr>
2024-10-25 10:20:05 +0200

    persit connection of the user

--- front/src/pages/SignIn/authSlice.jsx
+++ front/src/pages/SignIn/authSlice.jsx
@@ -1,24 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { userLogin } from "../../service/serviceApi";
 
+const token = localStorage.getItem("token");
+
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
-    token: null,
+    token: token ? token : null,
     error: null,
   },
   reducers: {
     logoutUser: (currentState) => {
       currentState.token = null;
+      localStorage.removeItem("token");
     },
   },
-  extraReducers: function (builder) {
+  extraReducers: (builder) => {
     builder.addCase(userLogin.fulfilled, (state, action) => {
       state.token = action.payload.body.token;
       state.error = "";
+      localStorage.setItem("token", action.payload.body.token);
     });
     builder.addCase(userLogin.rejected, (state, action) => {
       state.error = action.payload;
     });
   },
 });
+
+export const { logoutUser } = authSlice.actions;
+export default authSlice.reducer;
--- front/src/store.js
+++ front/src/store.js
@@ -1,8 +1,29 @@
+// store.js
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "./components/User/userSlice";
 import { authSlice } from "./pages/SignIn/authSlice";
 
-let state = {
+// Fonction pour charger l'état depuis localStorage
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    return serializedState ? JSON.parse(serializedState) : undefined;
+  } catch (e) {
+    console.error("Impossible de charger l'état depuis localStorage", e);
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (e) {
+    console.error("Impossible de sauvegarder l'état dans localStorage", e);
+  }
+};
+
+const preloadedState = loadState() || {
   auth: {
     token: null,
     error: null,
@@ -15,9 +36,13 @@ let state = {
 };
 
 export const store = configureStore({
-  preloadedState: state,
+  preloadedState,
   reducer: combineReducers({
     user: userSlice.reducer,
     auth: authSlice.reducer,
   }),
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
